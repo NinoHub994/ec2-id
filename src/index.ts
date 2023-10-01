75e748c67b957561ee2b2c5b55e2855951d25cef
NinoHub994 <antoninopirri94@gmail.com>
2023-10-01 12:40:41 +0200

    fix balancer params

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import http from 'http';
-import { MetadataService, AWSError } from 'aws-sdk'; // Import from AWS SDK v3
+import { config } from 'dotenv-flow';
+config({ silent: true });
+import AWS, { MetadataService, AWSError } from 'aws-sdk'; // Import from AWS SDK v3
 
 const metadata = new MetadataService();
 const app = express();
@@ -26,4 +28,38 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const dotenv = require('dotenv');
+
+// Carica le variabili di ambiente da un file .env
+dotenv.config();
+
+// Configura le credenziali AWS
+AWS.config.update({
+  region: process.env.AWS_REGION, // La regione AWS
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+// Crea un oggetto ELB
+const elbv2 = new AWS.ELBv2({ apiVersion: '2015-12-01' });
+
+// Esegui l'interazione con ELB qui, ad esempio, ottenere informazioni sul bilanciatore di carico
+const loadBalancerName = process.env.LOAD_BALANCER_NAME;
+
+if (loadBalancerName) {
+  const params = {
+    Names: [loadBalancerName],
+  };
+
+  elbv2.describeLoadBalancers(params, (err, data) => {
+    if (err) {
+      console.error('Errore durante la descrizione del bilanciatore di carico:', err);
+    } else {
+      console.log('Informazioni sul bilanciatore di carico:', data);
+    }
+  });
+} else {
+  console.error('Environment variable LOAD_BALANCER_NAME is not defined or is undefined.');
+}
 
