ab4b7a81381a898a47737160809354016a1469aa
NinoHub994 <antoninopirri94@gmail.com>
2023-10-09 20:46:51 +0200

    changed variable

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.get('/instance', (_, res) => {
 });
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4999;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
